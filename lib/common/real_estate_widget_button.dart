d099f55ed3b3232bfd05a2e50af6bee5113be9c0
Adole Samuel Sani <21035760+adolesamuel@users.noreply.github.com>
2024-05-19 17:44:06 +0100

    animating button slides done

--- lib/common/real_estate_widget_button.dart
+++ lib/common/real_estate_widget_button.dart
@@ -13,69 +13,79 @@ class RealEstateWidgetButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: const Color(0xFFD0BCA6).withOpacity(0.6),
-      clipBehavior: Clip.hardEdge,
-      borderRadius: BorderRadius.circular(100.r),
-      child: InkWell(
-        onTap: () {},
-        child: SizedBox(
-          height: 58,
-          child: Stack(
-            children: [
-              BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-                child: const SizedBox(
-                  height: 58,
-                ),
-              ),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      text,
-                      style: TextStyle(
-                        fontSize: 19.sp,
-                        color: AppColors.black,
-                        fontWeight: FontWeight.w500,
+    return TweenAnimationBuilder<double>(
+      tween: Tween<double>(begin: 1, end: 0),
+      curve: Curves.easeIn,
+      duration: const Duration(milliseconds: 300),
+      builder: (context, value, child) {
+        return Material(
+          color: const Color(0xFFD0BCA6).withOpacity(0.6),
+          clipBehavior: Clip.hardEdge,
+          borderRadius: BorderRadius.circular(100.r),
+          child: InkWell(
+            onTap: () {},
+            child: SizedBox(
+              height: 58,
+              child: Stack(
+                children: [
+                  BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                    child: const SizedBox(
+                      height: 58,
+                    ),
+                  ),
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          text,
+                          style: TextStyle(
+                            fontSize: 19.sp,
+                            color: AppColors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Transform.translate(
+                    offset: Offset(200 * -value, 0),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        height: 55,
+                        width: 58,
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 1.0,
+                          vertical: 2.0,
+                        ),
+                        decoration: const BoxDecoration(
+                          color: AppColors.orangeFBF,
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: AppColors.greyA5A,
+                              blurRadius: 14,
+                              spreadRadius: 8,
+                              offset: Offset(0, 0),
+                            )
+                          ],
+                        ),
+                        child: const Icon(
+                          Icons.chevron_right_outlined,
+                          color: AppColors.brown5A5,
+                        ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  height: 55,
-                  width: 58,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 1.0,
-                    vertical: 2.0,
-                  ),
-                  decoration: const BoxDecoration(
-                    color: AppColors.orangeFBF,
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: AppColors.greyA5A,
-                        blurRadius: 14,
-                        spreadRadius: 8,
-                        offset: Offset(0, 0),
-                      )
-                    ],
-                  ),
-                  child: const Icon(
-                    Icons.chevron_right_outlined,
-                    color: AppColors.brown5A5,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
