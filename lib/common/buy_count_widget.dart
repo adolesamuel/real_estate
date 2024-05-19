18536deb2bc2bd11be892a600462bd3ef77b2e16
Adole Samuel Sani <21035760+adolesamuel@users.noreply.github.com>
2024-05-19 17:25:44 +0100

    animated countdown widget

--- lib/common/buy_count_widget.dart
+++ lib/common/buy_count_widget.dart
@@ -1,3 +1,4 @@
+import 'package:countup/countup.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:moniepoint_real_estate/constants/app_colors.dart';
@@ -7,43 +8,57 @@ class BuyCountWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 220.r,
-      width: 220.r,
-      decoration: const BoxDecoration(
-        shape: BoxShape.circle,
-        color: AppColors.orangeFC9,
-      ),
-      child: Column(
-        children: [
-          20.verticalSpace,
-          Text(
-            'BUY',
-            style: TextStyle(
-              fontSize: 18.sp,
-              color: AppColors.whiteF8F,
-              fontWeight: FontWeight.w400,
+    return TweenAnimationBuilder<double>(
+        tween: Tween<double>(begin: 0, end: 1),
+        duration: const Duration(seconds: 1),
+        curve: Curves.easeInOutBack,
+        builder: (context, value, child) {
+          return Transform.scale(
+            scale: value,
+            child: Container(
+              height: 220.r,
+              width: 220.r,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.orangeFC9,
+              ),
+              child: Column(
+                children: [
+                  20.verticalSpace,
+                  Text(
+                    'BUY',
+                    style: TextStyle(
+                      fontSize: 18.sp,
+                      color: AppColors.whiteF8F,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  25.verticalSpace,
+                  Countup(
+                    begin: 0,
+                    end: 1034,
+                    duration: const Duration(seconds: 2),
+                    style: TextStyle(
+                      fontSize: 40.sp,
+                      color: AppColors.whiteF8F,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    separator: " ",
+                  ),
+                  Text(
+                    'offers',
+                    style: TextStyle(
+                      fontSize: 18.sp,
+                      color: AppColors.whiteF8F,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          25.verticalSpace,
-          Text(
-            "1 034",
-            style: TextStyle(
-              fontSize: 40.sp,
-              color: AppColors.whiteF8F,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            'offers',
-            style: TextStyle(
-              fontSize: 18.sp,
-              color: AppColors.whiteF8F,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-        ],
-      ),
-    );
+          );
+        });
   }
 }
+
+class Countdown {}
--- lib/common/rent_count_widget.dart
+++ lib/common/rent_count_widget.dart
@@ -1,3 +1,4 @@
+import 'package:countup/countup.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:moniepoint_real_estate/constants/app_colors.dart';
@@ -7,43 +8,56 @@ class RentCountWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 220.r,
-      width: 220.r,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(32.r),
-        color: AppColors.whiteF8F,
-      ),
-      child: Column(
-        children: [
-          20.verticalSpace,
-          Text(
-            'RENT',
-            style: TextStyle(
-              fontSize: 18.sp,
-              color: AppColors.brownA49,
-              fontWeight: FontWeight.w400,
+    return TweenAnimationBuilder<double>(
+      tween: Tween<double>(begin: 0, end: 1),
+      duration: const Duration(seconds: 1),
+      curve: Curves.easeInOutBack,
+      builder: (context, value, child) {
+        return Transform.scale(
+          scale: value,
+          child: Container(
+            height: 220.r,
+            width: 220.r,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(32.r),
+              color: AppColors.whiteF8F,
+            ),
+            child: Column(
+              children: [
+                20.verticalSpace,
+                Text(
+                  'RENT',
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    color: AppColors.brownA49,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                25.verticalSpace,
+                Countup(
+                  begin: 0,
+                  end: 2212,
+                  duration: const Duration(seconds: 2),
+                  style: TextStyle(
+                    fontSize: 40.sp,
+                    color: AppColors.brownA49,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  separator: " ",
+                ),
+                Text(
+                  'offers',
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    color: AppColors.brownA49,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
             ),
           ),
-          25.verticalSpace,
-          Text(
-            "2 212",
-            style: TextStyle(
-              fontSize: 40.sp,
-              color: AppColors.brownA49,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            'offers',
-            style: TextStyle(
-              fontSize: 18.sp,
-              color: AppColors.brownA49,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
